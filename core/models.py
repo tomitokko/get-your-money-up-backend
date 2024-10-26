f21da6e26b20f2553ed970da707bb54fd4f64b41
Tomi Tokko <tokkotomi09@gmail.com>
2024-10-26 16:53:05 +0100

    first

---
+++ core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    bank_statement_uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    date = models.DateField()
+    time = models.TimeField()
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=10)
+    description = models.TextField(blank=True, null=True)
+    money_out = models.DecimalField(max_digits=1000, decimal_places=2, blank=True, null=True)
+    money_in = models.DecimalField(max_digits=1000, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.category} - {self.amount} {self.currency}"
+
+class CategorisedTransaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    bank_statement_uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    category = models.CharField(max_length=255)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=10)
+
---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Transaction, CategorisedTransaction
+from .serializers import TransactionSerializer
+
+@api_view(['POST'])
+def create_transaction(request):
+    serializer = TransactionSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
